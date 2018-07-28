b78e525ef6d2d695a8664805c2bb8ca5f6a1db39
L-E-G-E-N-D <ojas.ajay.kulkarni@gmail.com>
2018-07-28 00:04:33 -0600

    Tested out functionality for the contact form, will be up and running after the website has been hosted. Until then, the code in the files changed for this functionality, are commented out!

---
+++ contactform.php
@@ -0,0 +1,19 @@
+ <?php
+/*    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $emailSubject = "New Form Submission";
+    $emailBody = "Name: $name.\n".
+                "Email: $mailFrom.\n".
+                "Subject: $subject.\n".
+                "Message: $message.\n";
+    $mailTo = "ojas.ajay.kulkarni@gmail.com";
+    $headers = "From: $mailFrom \r\n";
+    $headers .="Reply-To: $mailTo \r\n";
+
+    mail($mailTo, $emailSubject, $emailBody, $headers);
+
+    header("Location: index.php?mail-sent"); */
+   
--- header.php
+++ header.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Login System</title>
+    <title>Caliber - Online Fitness Tracking</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="style.css" />
     <script src="main.js"></script>
