0d7f64555d4a7379c0a1bd6ab5180281e4fefbe5
L-E-G-E-N-D <ojas.ajay.kulkarni@gmail.com>
2018-10-21 00:34:46 -0600

    Added All content to Home Page, imported fonts and background images, and got the desired font to work

--- header.php
+++ header.php
@@ -9,21 +9,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Caliber - Online Fitness Tracking</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+    <link rel="stylesheet" type="text/css" href="mainStyles.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto|Rubik" rel="stylesheet">
     <script src="main.js"></script>
 </head>
 <body>
     <header>
         <nav>
             <div class="main-wrapper">
-                <ul>
-                    <li><a href="index.php">Home</a></li>
-                    <li><button id="myBtn">Account Status<?php
-                        if (isset($_SESSION['u_id'])) {
-                            echo '<form action="includes/logout.inc.php" method="POST">
-                                    </form>';
-                        }
-                    ?></button></li>
+                <ul class="navigation-links">
+                    <li><a class="links" href="index.php">Home</a></li>
+                    <li><button class="links" id="myBtn">Account Status</button></li>
                 </ul>
                 <div class="nav-login">
                 </div>
--- index.php
+++ index.php
@@ -1,54 +1,9 @@
 <?php
     include_once 'header.php';
 ?>
-
-<section class="main-container">
-    <div class="main-wrapper">
-        <h2>Caliber Fitness</h2>
-        <img src="CaliberLogo.jpg">
-        <h1>The best way to improve your lives</h1>
-        <p>Caliber is the best fitness tracking application<br>developed to make your lives easier. Our products<br> have proved to keep everyone organized and on track!</p>
-        <a href="#">Contact Us</a>
-        <a href="#">Your Dashboard</a>
         <?php
             include_once 'signedin.php';
         ?>
-<style>
-.modal {
-    display: none; /* Hidden by default */
-    position: fixed; /* Stay in place */
-    z-index: 1; /* Sit on top */
-    padding-top: 100px; /* Location of the box */
-    left: 0;
-    top: 0;
-    width: 100%; /* Full width */
-    height: 100%; /* Full height */
-    overflow: auto; /* Enable scroll if needed */
-    background-color: rgb(0,0,0); /* Fallback color */
-    background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
-}
-.modal-content {
-    background-color: #fefefe;
-    margin: auto;
-    padding: 20px;
-    border: 1px solid #888;
-    width: 80%;
-}
-.close {
-    color: #aaaaaa;
-    float: right;
-    font-size: 28px;
-    font-weight: bold;
-}
-.close:hover,
-.close:focus {
-    color: #000;
-    text-decoration: none;
-    cursor: pointer;
-}
-</style>
-</head>
-<body>
 <!-- Trigger/Open The Modal -->
 
 <!-- The Modal -->
@@ -76,34 +31,69 @@
 </div>
 
 <script>
-// Get the modal
 var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
 }
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+// Start of the Add category code
+var exercise = 1;
+    exercise++;
+    var objTo = document.getElementById('room_fileds');
+    var divtest = document.createElement("div");
+    divtest.innerHTML = '<button id="openExerciseModal">Open Modal</button><h1 id="displayExerciseContentText"></h1><h1 id="displayExerciseContentReps"></h1><h1 id="displayExerciseContentSets"></h1><div id="ExerciseModal" class="modal"><div class="ExerciseModalContent"><span class="ExerciseModalClose">&times;</span><table><tr><td>Name of Exercise<input type="text" id="exerciseContent"></td></tr><tr><td>Repetitions<input type="text" id="exerciseReps"></td></tr><tr><td>Sets<input type="text" id="exerciseSets"></td></tr><br></table><button onclick="SaveExerciseContent()" id="saveClose" name="saveExer">Save Exercise</button></div></div>';
+    
+    objTo.appendChild(divtest);
+    var exerModal = document.getElementById('ExerciseModal');
+    var exerBtn = document.getElementById("openExerciseModal");
+    var saveButtonModalClose = document.getElementById("saveClose")[0];
+    var span = document.getElementsByClassName("ExerciseModalClose")[0];
+    exerBtn.onclick = function() {
+        exerModal.style.display = "block";
+    }
+    span.onclick = function() {
+        exerModal.style.display = "none";
+    }
+    saveButtonModalClose.onclick = function() {
+        exerModal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            exerModal.style.display = "none";
+        }
+    }
+    function SaveExerciseContent() {
+        var displayExerciseContent = document.getElementById("exerciseContent").value;
+        document.getElementById("displayExerciseContentText").innerHTML = displayExerciseContent;
+        var displayExerciseReps = document.getElementById("exerciseReps").value;
+        document.getElementById("displayExerciseContentReps").innerHTML = displayExerciseReps;
+        var displayExerciseSets = document.getElementById("exerciseSets").value;
+        document.getElementById("displayExerciseContentSets").innerHTML = displayExerciseSets;
+    }
+/* Start of the Opening Add Exercise Modal Code */
+
+/*// Start of the Save Text when clicked save inputs
+function SaveInputText() {
+    var text = document.getElementById("exerTitle").value;
+    document.getElementById("title").innerHTML = text;
+}
+*/
+function RemoveInputs() {
+    var inputRemove = document.getElementById("exerTitle");
+    var buttonRemove = document.getElementById("removeButton");
+    inputRemove.remove(inputRemove);
+    buttonRemove.remove(buttonRemove);
+}
 </script>  
-    </div>
-</section>
 <?php
     include_once 'footer.php';
 ?>
