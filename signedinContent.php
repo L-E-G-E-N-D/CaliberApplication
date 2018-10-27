edf02bea3b594676e8e3587b91837c8da3ba80b7
L-E-G-E-N-D <ojas.ajay.kulkarni@gmail.com>
2018-10-26 22:26:30 -0600

    Added Styles to main login page, and added functionality to add more exercises depending on need.

--- signedinContent.php
+++ signedinContent.php
@@ -2,7 +2,7 @@
     include_once 'header.php';
 ?>
 <div class="tracking-categories">
-    <a class="tracking-categories-home" href="index.php">Back to Home</a>
+    <a class="tracking-categories-home" href="index.php"><i class="fas fa-long-arrow-alt-left left_style"></i>Back to Home</a>
     <div id="room_fileds">
         <!-- <div class="container">
             <ul class="ks-cboxtags">
@@ -16,30 +16,31 @@
         </div> -->
     </div>
     <table>
-        <tr>
+        <tr class="style_row">
             <th>Name of Exercise</th>
             <th>Repetitions</th>
             <th>Sets</th>
         </tr>
         <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
+            <td id="exerciseNameDisplay">Alfreds Futterkiste</td>
+            <td id="exerciseRepsDisplay">Maria Anders</td>
+            <td id="exerciseSetsDisplay">Germany</td>
         </tr>
         <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
+            <td id="">Centro comercial Moctezuma</td>
+            <td id="">Francisco Chang</td>
+            <td id="">Mexico</td>
         </tr>
         <tr>
-            <td>Ernst Handel</td>
-            <td>Roland Mendel</td>
-            <td>Austria</td>
+            <td id="">Ernst Handel</td>
+            <td id="">Roland Mendel</td>
+            <td id="">Austria</td>
         </tr>
     </table>
+    <br>
 <div class="update_table">    
 <table>
-    <tr>
+    <tr class="style_row">
         <th>Anaerobic Exercises</th>
         <th>Aerobic Excercises</th>
         <th>Strength Excercises</th>
@@ -100,7 +101,7 @@
 </div>
 <div class="newOpenModalsWithId">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <button id="append">Add Exercise</button>
+    <button class="addMoreExerStyle" id="append">Add Exercise</button>
     <div id="parent"></div>
 <div id="parent"></div>
 </div>
@@ -112,12 +113,13 @@ var exercise = 1;
 $(function(){
   var count = 0;
   $('#append').click(function(){
-    $('#parent').append('<button id="openExerciseModal">Open Modal</button><h3 id="displayExerciseContentText"></h3><h3 id="displayExerciseContentReps"></h3><h3 id="displayExerciseContentSets"></h3><div id="ExerciseModal" class="modal"><div class="ExerciseModalContent"><span class="ExerciseModalClose">&times;</span><table><tr><td>Name of Exercise<input type="text" id="exerciseContent"></td></tr><tr><td>Repetitions<input type="text" id="exerciseReps"></td></tr><tr><td>Sets<input type="text" id="exerciseSets"></td></tr><br></table><button onclick="SaveExerciseContent()" id="saveClose" name="saveExer">Save Exercise</button><div class="container"><ul class="ks-cboxtags"><li><input type="checkbox" onclick="SaveExercises()" id="checkboxOne"><label for="checkboxOne">Anaerobic Exercise</label></li><li><input type="checkbox" onclick="SaveExercises()" id="checkboxTwo"><label for="checkboxTwo">Aerobic Excercise</label></li><li><input type="checkbox" onclick="SaveExercises()" id="checkboxThree"><label for="checkboxThree">Strength Excercise</label></li><li><input type="checkbox" onclick="SaveExercises()" id="checkboxFour"><label for="checkboxFour">Cardio Excercise</label></li><li><input type="checkbox" onclick="SaveExercises()" id="checkboxFive"><label for="checkboxFive">Core Training</label></li><li><input type="checkbox" onclick="SaveExercises()" id="checkboxSix"><label for="checkboxSix">Stretching</label></li></ul></div></div></div>'+count+'"><br>');
+    var AddButton = '<button class="openAnotherClass" id="openExerciseModal">Add Exercise Details</button><h3 id="displayExerciseContentText"></h3><h3 id="displayExerciseContentReps"></h3><h3 id="displayExerciseContentSets"></h3><div id="ExerciseModal" class="modal"><div class="ExerciseModalContent"><span class="ExerciseModalClose">&times;</span><table><tr><td>Exercise<input type="text" class="exerStyle" id="exerciseContent"></td></tr><tr><td>Repetitions<input type="text" class="exerStyle" id="exerciseReps"></td></tr><tr><td>Sets<input type="text" class="exerStyle" id="exerciseSets"></td></tr><br></table><button onclick="SaveExerciseContent()" class="saveStyle" id="saveClose" name="saveExer">Save Exercise</button><div class="container"><ul class="ks-cboxtags"><li><input type="checkbox" onclick="SaveExercises()" id="checkboxOne"><label for="checkboxOne">Anaerobic Exercise</label></li><li><input type="checkbox" onclick="SaveExercises()" id="checkboxTwo"><label for="checkboxTwo">Aerobic Excercise</label></li><li><input type="checkbox" onclick="SaveExercises()" id="checkboxThree"><label for="checkboxThree">Strength Excercise</label></li><li><input type="checkbox" onclick="SaveExercises()" id="checkboxFour"><label for="checkboxFour">Cardio Excercise</label></li><li><input type="checkbox" onclick="SaveExercises()" id="checkboxFive"><label for="checkboxFive">Core Training</label></li><li><input type="checkbox" onclick="SaveExercises()" id="checkboxSix"><label for="checkboxSix">Stretching</label></li></ul></div></div></div'+count+'"><br>';
+    $('#parent').append(AddButton);
     count++;
   });
 });
     
-objTo.appendChild(divtest);
+    objTo.appendChild(divtest);
     var exerModal = document.getElementById('ExerciseModal');
     var exerBtn = document.getElementById("openExerciseModal");
     var saveButtonModalClose = document.getElementById("saveClose")[0];
@@ -158,14 +160,14 @@ objTo.appendChild(divtest);
     }
     function SaveExerciseContent() {
         var displayExerciseContent = document.getElementById("exerciseContent").value;
-        document.getElementById("displayExerciseContentText").innerHTML = displayExerciseContent;
+        document.getElementById("exerciseNameDisplay").innerHTML = displayExerciseContent;
         var displayExerciseReps = document.getElementById("exerciseReps").value;
-        document.getElementById("displayExerciseContentReps").innerHTML = displayExerciseReps;
+        document.getElementById("exerciseRepsDisplay").innerHTML = displayExerciseReps;
         var displayExerciseSets = document.getElementById("exerciseSets").value;
-        document.getElementById("displayExerciseContentSets").innerHTML = displayExerciseSets;
+        document.getElementById("exerciseSetsDisplay").innerHTML = displayExerciseSets;
     }
 </script>
-
+<!--
 <div class="tracking-items">
     <button class="tracking-items-change" name="changetheme">Change Theme</button>
-</div>
+</div> -->
